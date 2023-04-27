2d65ce023bd40274d239f865b1f94338910c3b92
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-04-27 18:58:09 -0300

    App portafolio v1.5

--- src/components/layouts/desktop.js
+++ src/components/layouts/desktop.js
@@ -3,6 +3,7 @@ import React from "react";
 import ComponentMenuLeft from "../partials/desktop/menu_left.js";
 import ComponentInicio from "../partials/desktop/inicio.js";
 import ComponentSobreMi from "../partials/desktop/sobre_mi.js";
+import ComponentServices from "../partials/desktop/services.js";
 import ComponentRedesSociales from "../partials/desktop/redes_sociales.js";
 
 export default function ComponentIsDeskatop(){
@@ -11,6 +12,7 @@ export default function ComponentIsDeskatop(){
             <ComponentMenuLeft/>
             <ComponentInicio/>
             <ComponentSobreMi/>
+            <ComponentServices/>
             <ComponentRedesSociales/>
         </section>
     )
---
+++ src/components/partials/desktop/services.js
@@ -0,0 +1,37 @@
+import style from '../../../styles/partials/desktop/services.module.css';
+
+export default function ComponentServices(){
+    return (
+        <a id="servicios" className={style.container}>
+            <h2 className={style.title}>
+                <span>
+                    Servicios
+                </span>
+                <ion-icon name="book-outline"></ion-icon>
+            </h2>
+            <article className={style.description}>
+                <div className={style.item}>
+                    <div className={style.icon}>
+                        
+                    </div>
+                    <h3>Diseño Web</h3>
+                    <p>Realizo maquetados web basándome en resultados de diseñadores gráficos, implementando herramientas de diseño profesional como Photoshop, Ilustrator y Lightroom.</p>
+                </div>
+                <div className={style.item}>
+                    <div className={style.icon}>
+                        <ion-icon name="code-working-outline"></ion-icon>
+                    </div>
+                    <h3>Programacion Especifica</h3>
+                    <p>Soluciones personalizadas para necesidades más complejas y puntuales.</p>
+                </div>
+                <div className={style.item}>
+                    <div className={style.icon}>
+                        <i class='bx bxs-data'></i>
+                    </div>
+                    <h3>Bases de datos</h3>
+                    <p>Realizo implementaciones de bases de datos junto al desarrollo de aplicaciones en plataformas como MySQL, Microsoft SQL Server y MongoDB.</p>
+                </div>
+            </article>
+        </a>
+    )
+}
