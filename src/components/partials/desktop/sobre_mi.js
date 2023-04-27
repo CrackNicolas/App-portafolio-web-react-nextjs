af7c9d99647bce1b50329bb1a7da195f55d09408
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-04-27 18:30:42 -0300

    App portafolio v1.4

--- src/components/partials/desktop/sobre_mi.js
+++ src/components/partials/desktop/sobre_mi.js
@@ -3,13 +3,320 @@ import style from '../../../styles/partials/desktop/sobre_mi.module.css';
 export default function ComponentSobreMi(){
     return (
         <a id="sobre-mi" className={style.container}>
-            <h2>Sobre mi</h2>
-            <h4>¿Por qué me contratan?</h4>
-            <p>
+            <h2 className={style.title}>
+                <span>
+                    Sobre mi
+                </span>
+                <ion-icon name="person-outline"></ion-icon>
+            </h2>
+            <h4 className={style.contratan}>¿Por qué me contratan?</h4>
+            <p className={style.description}>
                 Soy un profesional dedicado a la excelencia de mis proyectos, recomendando tecnologías y estrategias para cada caso.
                 <br/>
                 Mantenerme a la vanguardia es una de mis principales prioridades.
             </p>
+            <article className={style.habilidades}>
+            <div className={style.porcentaje}>
+                <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class="lab la-mdb bx-sm"></i>
+                        <h2>
+                            90
+                            <span>%</span>
+                        </h2>
+                        <p>MySql</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class="lab la-bootstrap bx-sm"></i>
+                        <h2>
+                            90
+                            <span>%</span>
+                        </h2>
+                        <p>Bootstrap</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <ion-icon name="logo-react"></ion-icon>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>React</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class="lab la-java bx-sm"></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>Java</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <ion-icon name="logo-angular"></ion-icon>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>Angular</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class='bx bxl-typescript bx-sm'></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>TypeScript</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <ion-icon name="logo-css3"></ion-icon>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>CSS</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <ion-icon name="logo-html5"></ion-icon>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>HTML</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class="lab la-php bx-sm"></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>PHP</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <ion-icon name="logo-npm"></ion-icon>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>npm</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class="lab la-node bx-sm"></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>Node.js</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <ion-icon name="logo-javascript"></ion-icon>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>JavaScript</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class='bx bxl-tailwind-css bx-sm'></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>Tailwind</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class='bx bxl-c-plus-plus bx-sm'></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>C/C++</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class='bx bxl-mongodb bx-sm'></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>MongoDB</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class='bx bxl-jquery bx-sm'></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>jQuery</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <ion-icon name="infinite-outline"></ion-icon>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>Arduino</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <ion-icon name="logo-vercel"></ion-icon>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>Next.js</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class="lab la-jira bx-sm"></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>Jira</p>
+                    </div>
+                </div>
+                <div className={style.porcentaje}>
+                    <div className={style.dot}></div>
+                    <svg>
+                        <circle cx="50" cy="50" r="50"></circle>
+                        <circle cx="50" cy="50" r="50"></circle>
+                    </svg>
+                    <div className={style.number}>
+                        <i class="lab la-trello bx-sm"></i>
+                        <h2>
+                            85
+                            <span>%</span>
+                        </h2>
+                        <p>Trello</p>
+                    </div>
+                </div>
+            </article>
         </a>
     )
 }
