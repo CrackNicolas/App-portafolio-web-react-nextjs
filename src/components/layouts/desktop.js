c21ea3834fb679f34007885dff8f708dde21d304
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-04-28 10:53:02 -0300

    App portafolio v1.6

--- src/components/layouts/desktop.js
+++ src/components/layouts/desktop.js
@@ -4,6 +4,7 @@ import ComponentMenuLeft from "../partials/desktop/menu_left.js";
 import ComponentInicio from "../partials/desktop/inicio.js";
 import ComponentSobreMi from "../partials/desktop/sobre_mi.js";
 import ComponentServices from "../partials/desktop/services.js";
+import ComponentContactar from "../partials/desktop/contactar.js";
 import ComponentFooter from "../partials/desktop/footer.js";
 import ComponentRedesSociales from "../partials/desktop/redes_sociales.js";
 
@@ -14,6 +15,7 @@ export default function ComponentIsDeskatop(){
             <ComponentInicio/>
             <ComponentSobreMi/>
             <ComponentServices/>
+            <ComponentContactar/>
             <ComponentFooter/>
             <ComponentRedesSociales/>
         </section>
---
+++ src/components/partials/desktop/contactar.js
@@ -0,0 +1,29 @@
+import style from '../../../styles/partials/desktop/contactar.module.css';
+
+export default function ComponentContactar(){
+    return (
+        <a id="contactar" className={style.container}>
+            <h2 className={style.title}>
+                <span>
+                    Contactame
+                </span>
+                <ion-icon name="mail-outline"></ion-icon>
+            </h2>
+            <article className={style.form}>
+                <h2>Formulario de contacto</h2>
+                <form action="" method="POST">
+                    <div>
+                        <input type="text" placeholder="Nombre..."></input>
+                        <input type="text" placeholder="Apellido..."></input>
+                    </div>
+                    <input type="text" placeholder="Correo..."></input>
+                    <textarea type="text" placeholder="Consulta..."/>
+                    <button type="submit">
+                        <ion-icon name="send-outline"></ion-icon>
+                        <span>Contactar</span>
+                    </button>
+                </form>
+            </article>
+        </a>
+    )
+}
