8eb73f16051d8e65ecd48b59afc2e68df39433fb
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-05-07 09:04:18 -0300

    App portafolio v3.7

--- src/components/partials/contactar.js
+++ src/components/partials/contactar.js
@@ -1,10 +1,37 @@
+import emailjs from '@emailjs/browser';
+import ReCAPTCHA from "react-google-recaptcha";
+
+import { useRef, useState } from "react";
 import { useTranslation } from 'react-i18next';
-import { useForm, ValidationError } from '@formspree/react';
+import { key_global_emailjs, key_service_emailjs, key_template_emaijs, key_recaptcha } from '../../config/credentials.js';
+
 import style from '../../styles/partials/contactar.module.css';
+import { useForm } from 'react-hook-form';
 
 export default function ComponentContactar(){
     const {t} = useTranslation();
-    const [state, handleSubmit] = useForm("xvonwlew");
+    
+    const {register, formState : { errors },handleSubmit} = useForm();
+
+    const [state_captcha,setState_captcha] = useState(null);
+    const ref_form = useRef(null);
+    const recaptcha = useRef(null);
+
+    const onSubmit = () => {
+        if(state_captcha){
+            /*emailjs.sendForm(
+                key_service_emailjs,
+                key_template_emaijs,
+                ref_form.current,
+                key_global_emailjs
+            ).then(result => console.log(result.text)).catch(error => console.log(error))
+            */
+        }
+    }
+
+    const onChange = () => {
+        setState_captcha(recaptcha.current.getValue())
+    }
 
     return (
         <section id="contactar" className={style.container}>
@@ -14,18 +41,43 @@ export default function ComponentContactar(){
             </h2>
             <article className={style.form}>
                 <h2> { t('contact.subtitle') } </h2>
-                <form action="" method="POST" onSubmit={handleSubmit}>
+                <form action="" method="POST" ref={ref_form} onSubmit={handleSubmit(onSubmit)}>
                     <div>
-                        <input type="text" id="name" name="name" placeholder={ t('contact.inputs.j_1') } />
-                        <ValidationError prefix="Name" field="name" errors={state.errors} />
-                        <input type="text" id="last_name" name="last_name" placeholder={ t('contact.inputs.j_2') } />
-                        <ValidationError prefix="Name" field="name" errors={state.errors} />
+                        <input type="text" placeholder={ t('contact.inputs.j_1') } {...register('name', {
+                            required : true,
+                            maxLength : 10
+                        })}  />
+                        {errors.name?.type === "required" && <p>El nombre es requerido</p> }
+                        {errors.name?.type === "maxLength" && <p>El nombre debe tener menos de 10 caracteres</p> }
+                        <input type="text" placeholder={ t('contact.inputs.j_2') } {...register('last_name',{
+                            required : true,
+                            maxLength : 10
+                        })} />
+                        {errors.last_name?.type === "required" && <p>El apellido es requerido</p> }
+                        {errors.last_name?.type === "maxLength" && <p>El apellido debe tener menos de 10 caracteres</p> }
                     </div>
-                    <input type="email" id="email" name="email" placeholder={ t('contact.inputs.j_3') } />
-                    <ValidationError prefix="Email" field="email" errors={state.errors} />
-                    <textarea type="text" id="message" name="message" placeholder={ t('contact.inputs.j_4') }/>
-                    <ValidationError prefix="Message" field="message" errors={state.errors} />
-                    <button type="submit" disabled={state.submitting}>
+                    <input type="email" placeholder={ t('contact.inputs.j_3') } {...register('email',{
+                        required : true,
+                        pattern : /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
+                    })} />
+                    {errors.email?.type === "required" && <p>El email es requerido</p> }
+                    {errors.email?.type === "pattern" && <p>Email incorrecto</p> }
+                    <textarea type="text" placeholder={ t('contact.inputs.j_4') } {...register('message',{
+                        required : true
+                    })}/>
+                    {errors.message?.type === "required" && <p>El mensaje es requerido</p> }
+                    {errors.message?.type === "maxLength" && <p>El mensaje debe tener menos de 10 caracteres</p> }
+                    <aside className={style.recaptcha}>
+                        <div>
+                            <ReCAPTCHA
+                                ref={recaptcha}
+                                sitekey={key_recaptcha}
+                                onChange={onChange}
+                            />
+                        </div>
+                        {recaptcha.current?.getValue() == false && <p>Te faltó validar que no sos un robot.</p>}
+                    </aside>
+                    <button type="submit">
                         <ion-icon name="send-outline"></ion-icon>
                         <span>{ t('menu.i_6') }</span>
                     </button>
---
+++ src/config/credentials.js
@@ -0,0 +1,4 @@
+export const key_recaptcha = "6LfoLOklAAAAAMZ0rM3pBfmzxa6Kj47b5EIvA45C";
+export const key_global_emailjs = "BN1MU6VxFU7x0E6Ne";
+export const key_service_emailjs = "service_88b3cyb";
+export const key_template_emaijs = "template_msbebk6";
