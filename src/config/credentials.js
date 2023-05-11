f3f13b773f8ab3336658f0ec0543724cc43df77f
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-05-11 07:54:12 -0300

    App portafolio v4.1

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,6 +1,7 @@
 const env = process.env.NODE_ENV;
 
-export const key_recaptcha = (env == "development")? "6LfoLOklAAAAAMZ0rM3pBfmzxa6Kj47b5EIvA45C" : (env == "production")? "" : "";
+//export const key_recaptcha = (env == "development")? "6LfoLOklAAAAAMZ0rM3pBfmzxa6Kj47b5EIvA45C" : (env == "production")? "" : "";
+export const key_recaptcha = "6LfoLOklAAAAAMZ0rM3pBfmzxa6Kj47b5EIvA45C";
 export const key_global_emailjs = "BN1MU6VxFU7x0E6Ne";
 export const key_service_emailjs = "service_88b3cyb";
 export const key_template_emaijs = "template_msbebk6";
